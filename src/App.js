81c2db37b8f0d878d048f5623b51405968bd5297
pranoy <chandra.pranay@yahoo.in>
2022-10-05 13:47:36 +0530

    header styling complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from './components/header/Header';
 
 function App() {
+
   return (
     <div className="App">
       <Router>
