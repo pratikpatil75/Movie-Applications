01c7528a86a0f27d96eba1ed251b642758afb316
pranoy <chandra.pranay@yahoo.in>
2022-10-05 17:38:32 +0530

    added the footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from './components/header/Header';
 import MovieList from './components/movieList/movieList'
 import Home from './pages/home/Home';
+import Movie from './pages/movieDetail/movieDetail';
+import Footer from './components/footer/Footer';
 
 function App() {
 
@@ -13,10 +15,11 @@ function App() {
         <Header />
         <Routes>
           <Route index element={<Home />}></Route>
-          <Route path='movie/:id' element={<h1>Movie Detail Page</h1>}></Route>
+          <Route path='movie/:id' element={<Movie />}></Route>
           <Route path='movies/:type' element={<MovieList />}></Route>
           <Route path='/*' element={<h1>Error Page</h1>}></Route>
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
